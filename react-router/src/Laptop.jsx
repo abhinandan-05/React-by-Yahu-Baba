6df4b27e01a5f2fb8f017911fa9dfa3a9e172211
Abhinandan Kumar <abhicpr05@gmail.com>
2025-11-22 13:49:24 +0530

    React router

---
+++ react-router/src/Laptop.jsx
@@ -0,0 +1,14 @@
+function Phone() {
+  return (
+    <div>
+      <h1>Phone List :</h1>
+      <ul>
+        <li>Apple Macbook</li>
+        <li>Samsung Notebook</li>
+        <li>Google Inspiron</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Phone;
